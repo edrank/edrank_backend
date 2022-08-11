c61badcfb161ceca1682e6a4ed8871a1632eb57a
Om Gupta <omgupta1608@gmail.com>
2022-08-11 10:39:35 +0530

    basic mysql setup in ml_engine

--- ml_engine/app.py
+++ ml_engine/app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import jsonify, Flask
+import pymysql
+from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Edrank ML Engine v1.0'
+mysql = MySQL()
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_HOST'] = '185.210.145.52'
+app.config['MYSQL_DATABASE_USER'] = 'u341279852_edrank_dev'
+app.config['MYSQL_DATABASE_PASSWORD'] = '#Edrank@SIH22#'
+app.config['MYSQL_DATABASE_DB'] = 'u341279852_edrank_db'
+
+mysql.init_app(app)
+
+@app.route('/')
+def users():
+    conn = mysql.connect()
+
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute('SELECT * FROM teachers')
+
+    rows = cursor.fetchall()
+
+    resp = jsonify(rows)
+    resp.status_code = 200
+
+    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5002)
