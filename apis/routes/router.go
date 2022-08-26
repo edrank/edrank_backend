242b05fe53108d573ddc05c8a385745220d65991
Om Gupta <omgupta1608@gmail.com>
2022-08-26 17:15:45 +0530

    removed verify tenants

--- apis/routes/router.go
+++ apis/routes/router.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github.com/edrank/edrank_backend/apis/controllers"
-	"github.com/edrank/edrank_backend/apis/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -42,38 +41,38 @@ func InitPrivateRoutes(r *gin.RouterGroup) {
 	r.GET("/my-profile", controllers.GetMyProfile)
 	r.POST("/top-n-teachers", controllers.TopNTeachersController)
 	r.POST("/top-n-colleges", controllers.TopNCollegesController)
-	r.POST("/feedback-questions/:type", middlewares.VerifyTenants([]string{"PARENT", "STUDENT", "HEI", "COLLEGE_ADMIN"}), controllers.GetFeedbackQuestionsController)
-	r.POST("/submit-feedback/:type", middlewares.VerifyTenants([]string{"STUDENT", "PARENT", "HEI"}), controllers.SubmitFeedbackController)
-	r.POST("/get-feedback-teachers", middlewares.VerifyTenants([]string{"STUDENT"}), controllers.GetFeedbackTeachersController)
-	r.POST("/get-my-colleges-rank", middlewares.VerifyTenants([]string{"STUDENT", "TEACHER", "PARENT", "SUPER_ADMIN"}), controllers.GetMyCollegesRankController)
+	r.POST("/feedback-questions/:type", controllers.GetFeedbackQuestionsController)
+	r.POST("/submit-feedback/:type", controllers.SubmitFeedbackController)
+	r.POST("/get-feedback-teachers", controllers.GetFeedbackTeachersController)
+	r.POST("/get-my-colleges-rank", controllers.GetMyCollegesRankController)
 	r.GET("/get-course/:id", controllers.GetCourse)
 	r.POST("/submit-gc", controllers.SubmitGCFormController)
 
 	// college admin APIs
-	r.POST("/onboard-college", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.OnBoardCollegeController)
-	r.POST("/create-college-admin", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.CreateNewCollgeAdminController)
-	r.GET("/my-college-entity/:entity", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.GetEntitiesOfMyCollegeController)
-	r.POST("/toggle-feedback-drive", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.ToggleFeedbackDriveController)
-	r.GET("/dashboard-metrics", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.GetDashboardMetricsController)
-	r.GET("/get-college-drives", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN"}), controllers.GetCollegeFeedbackDrivesController)
+	r.POST("/onboard-college", controllers.OnBoardCollegeController)
+	r.POST("/create-college-admin", controllers.CreateNewCollgeAdminController)
+	r.GET("/my-college-entity/:entity", controllers.GetEntitiesOfMyCollegeController)
+	r.POST("/toggle-feedback-drive", controllers.ToggleFeedbackDriveController)
+	r.GET("/dashboard-metrics", controllers.GetDashboardMetricsController)
+	r.GET("/get-college-drives", controllers.GetCollegeFeedbackDrivesController)
 	r.GET("/get-teacher-linking-data", controllers.GetTeacherLinkingDataController)
 	// parent APIs
-	r.GET("/get-my-students", middlewares.VerifyTenants([]string{"PARENT"}), controllers.GetStudentsOfParent)
+	r.GET("/get-my-students", controllers.GetStudentsOfParent)
 
 	// teacher APIs
-	r.GET("/get-my-text-feedbacks", middlewares.VerifyTenants([]string{"TEACHER"}), controllers.GetMyTextFeedbacksController)
-	r.GET("/get-my-rank/:rank_type", middlewares.VerifyTenants([]string{"TEACHER"}), controllers.GetMyRankController)
+	r.GET("/get-my-text-feedbacks", controllers.GetMyTextFeedbacksController)
+	r.GET("/get-my-rank/:rank_type", controllers.GetMyRankController)
 
 	// android specific APIs
-	r.POST("/android/submit-feedback/:type", middlewares.VerifyTenants([]string{"STUDENT", "PARENT", "HEI"}), controllers.AndroidSubmitFeedbackController)
+	r.POST("/android/submit-feedback/:type", controllers.AndroidSubmitFeedbackController)
 
 	// regulator APIs
-	r.POST("/regulator/get-colleges", middlewares.VerifyTenants([]string{"SUPER_ADMIN"}), controllers.GetRegulatorCollegesController)
-	r.POST("/detailed-feedback", middlewares.VerifyTenants([]string{"SUPER_ADMIN"}), controllers.GetDetailedFeedbackController)
+	r.POST("/regulator/get-colleges", controllers.GetRegulatorCollegesController)
+	r.POST("/detailed-feedback", controllers.GetDetailedFeedbackController)
 	r.POST("/get-college", controllers.GetMyCollegeForNonCollegeController)
 
 	// analytics APIs
-	r.POST("/kbc-graph", middlewares.VerifyTenants([]string{"COLLEGE_ADMIN", "TEACHER"}), controllers.KBCGraphController)
+	r.POST("/kbc-graph", controllers.KBCGraphController)
 	r.POST("/sa-graph/:type", controllers.GetSAGraphController)
 	r.GET("/progress-graph/:tid", controllers.GetProgessGraph)
 }
